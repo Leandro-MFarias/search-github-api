6a59faf6a02de82a1586717eae18deb158750e28
Leandro Farias <leandrorf1606@gmail.com>
2024-02-19 14:48:46 -0300

    eventos-do-tipo-create-e-push

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,32 +1,38 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
-        this.userProfile.innerHTML = `
-                                     <div class="info">
-                                        <img src="${user.avatarUrl}" alt="Foto do Perfil de Usuário" />
-                                        <div class="data">
-                                            <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
-                                            <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
-                                            <div class="follows">
-                                                <p>Seguidores ${user.myFollowers}</p>
-                                                <p>Seguindo ${user.iFollowing}</p>
-                                            </div>
-                                        </div>
-                                     </div>`
+        this.userProfile.innerHTML =
+            `<div class="info">
+                <img src="${user.avatarUrl}" alt="Foto do Perfil de Usuário" />
+                
+                <div class="data">
+                    <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                    <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
+                    <div class="follows">
+                        <p>Seguidores ${user.myFollowers}</p>
+                        <p>Seguindo ${user.iFollowing}</p>
+                    </div>
+                </div>
+            /div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a class="repo-link" href="${repo.html_url}" target="_blank">${repo.name}<br><br>
-        <p class="repo-info">🍴${repo.forks}</p> 
-        <p class="repo-info">⭐${repo.stargazers_count}</p>
-        <p class="repo-info">👀${repo.watchers}</p>
-        <p class="repo-info">👨‍💻${repo.language}</p> </a><li>`)
+        user.repositories.forEach(repo => repositoriesItens +=
+            `<li>
+                <a class="repo-link" href="${repo.html_url}" target="_blank">${repo.name}
+                <br><br>
+                    <p class="repo-info">🍴${repo.forks}</p> 
+                    <p class="repo-info">⭐${repo.stargazers_count}</p>
+                    <p class="repo-info">👀${repo.watchers}</p>
+                    <p class="repo-info">👨‍💻${repo.language ?? 'Não possuie tec'}</p> 
+                </a>
+            <li>`)
 
         if (user.repositories.length > 0) {
-            this.userProfile.innerHTML += `
-                                                <div class="repositories section">
-                                                    <h2>Repositórios</h2>
-                                                    <ul>${repositoriesItens}</ul>
-                                                </div>`
+            this.userProfile.innerHTML +=
+                `<div class="repositories section">
+                    <h2>Repositórios</h2>
+                    <ul>${repositoriesItens}</ul>
+                </div>`
         }
 
         let eventItens = ''
@@ -46,11 +52,11 @@ const screen = {
                 <ul>${eventItens}</ul>
             </div>`
         } else {
-            this.userProfile.innerHTML += `
-                                            <div class="events section">
-                                                <h2>Eventos</h2>
-                                                <p>Nenhum evento encontrado.</p>
-                                            </div>`
+            this.userProfile.innerHTML += 
+            `<div class="events section">
+                <h2>Eventos</h2>
+                <p>Nenhum evento encontrado.</p>
+            </div>`
         }
     },
 
